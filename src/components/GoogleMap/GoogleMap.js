5fc19242ea2ac747e6cd36952d8eeec9c1aed1b2
Garcia <abby.garcia@nycfhc-nmx13319.na.corp.ipgnetwork.com>
2017-08-09 16:28:21 -0400

    merr maps is kinda there

--- src/components/GoogleMap/GoogleMap.js
+++ src/components/GoogleMap/GoogleMap.js
@@ -8,12 +8,19 @@ class GoogleMaps extends Component {
 				<div className='mapperText right'>
 				   <h3>Broadway Show Mapper</h3>
 					 <p>Where is the theater?</p>
+
+
+
+
+
+
 				</div>
       </div>
     );
   }
 }
 
+// export default GoogleMaps;
 export default GoogleMaps;
 
 
@@ -23,3 +30,18 @@ export default GoogleMaps;
 
 
 // Google API key AIzaSyBXfW8XcDqvlnbJ1OjNrAS4s9KS0Y0ZJlk
+
+// tom chen's google map stuff begins
+			 // 		<GoogleMap
+			 // 		// ref={props.onMapLoad}
+			 // 		defaultZoom={3}
+			 // 		defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
+			 // 		// onClick={props.onMapClick}>
+			 // 		{props.markers.map((marker, index) => (
+			 // 		<Marker
+			 // 		{...marker}/>
+			 // 		)
+			 // )}
+			 // 		</GoogleMap>
+
+//tom chen's google map stuff ends
